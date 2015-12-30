57402d1777251ef9b5ecdb8891279ccd8c0f4236
Clayton Walker <claytonwalkerucf@knights.ucf.edu>
2015-12-29 22:54:38 -0500

    Initial commit for 1 LED working with push button

---
+++ TrafficLight/TrafficLight.ino
@@ -0,0 +1,27 @@
+/*
+ * Clayton Walker
+ * TrafficLight.ino
+*/
+
+int ledPin = 5;
+int buttonPin = 4;
+
+bool ledState = LOW;
+bool lastState = LOW;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  //check for button push
+  bool buttonState = digitalRead(buttonPin);
+
+  //if button was pushed, change state of LED
+  if(buttonState == HIGH && lastState == LOW){
+    
+  }
+  digitalWrite(ledPin, ledState);
+}
